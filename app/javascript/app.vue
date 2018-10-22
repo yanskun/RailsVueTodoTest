3c606cb0bd415a57e431014c35c41596bcbbce4b
yasudanaoya <yasudanaoya@yasudanaoya.local>
2018-10-22 17:42:22 +0900

    ローカル環境の復元が完了

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,22 +1,81 @@
 <template>
-  <div id="app">
-    <p>{{ message }}</p>
+  <div id="app" class="container">
+    <ul>
+      <!-- タイトル -->
+      <h2>Your Tasks</h2>
+      <!-- ドラック追加 -->
+      <draggable :options="{group:'ITEMS'}">
+        <li v-for="(todo, index) in todos">
+          <a>{{ todo }}</a>
+          <a @click="deleatTodo(index)">[x]</a>
+        </li>
+      </draggable>
+    </ul>
+      <form @submit.prevent>
+        <input type="text" v-model="newTodo" placeholder="Enter your new task">
+        <button class="btn-add" @click="addTodo">add</button>
+      </form>
+    <ul>
+      <h2>Working Tasks</h2>
+      <draggable :options="{group:'ITEMS'}">
+        <li v-for="(wtodo, index) in wtodos">
+          <a>{{ wtodo }}</a>
+          <a @click="deleatTodo(index)">[x]</a>
+        </li>
+      </draggable>
+    </ul>
+    <ul>
+      <h2>Finish Tasks</h2>
+      <draggable :options="{group:'ITEMS'}">
+        <li v-for="(ftodo, index) in ftodos">
+          <a>{{ ftodo }}</a>
+          <a @click="deleatTodo(index)">[x]</a>
+        </li>
+      </draggable>
+    </ul>
   </div>
 </template>
 
 <script>
+// draggable
+import draggable from 'vuedraggable';
+
 export default {
+  name: 'app',
+  components: {
+    draggable,
+  },
   data: function () {
     return {
-      message: "Hello Vue!"
+      newTodo: '',
+      todos: [
+        'Task01',
+        'Task02',
+        'Task03',
+      ],
+      wtodos :[
+        'Task11',
+        'Task12',
+      ],
+      ftodos: [
+        'Task21',
+      ]
+    }
+  },
+    methods: {
+      addTodo:  function() {
+        this.todos.push(this.newTodo);
+        this.newTodo = '';
+      },
+      deleatTodo: function(index) {
+        if (confirm('Are you sure?')) {
+          this.todos.splice(index, 1)
+        }
+      }
     }
   }
-}
 </script>
 
 <style scoped>
-p {
-  font-size: 2em;
-  text-align: center;
-}
+
 </style>
