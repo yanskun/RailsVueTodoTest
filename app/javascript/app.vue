bfa09313c036d6e9dab8802c4df54080560d0594
Naoya Yasuda <naoya.yasuda@seattleconsulting.co.jp>
2018-10-24 14:48:18 +0900

    add:header template

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,13 +1,27 @@
 <template>
-  <div>
-    <h1>Trello()</h1>
-    <ul>
-      <li><router-link to="/">Task</router-link></li>
-      <li><router-link to="/about">About</router-link></li>
-    </ul>
+  <div id="temp">
+    <header>
+      <h1>Trello()</h1>
+      <nav>
+      <ul>
+        <li><router-link to="/">Task</router-link></li>
+        <li><router-link to="/about">About</router-link></li>
+      </ul>
+      </nav>
+    </header>
   </div>
 </template>
 
 <style>
+
+header {
+  background-color: rgba(150, 187, 142, 0.9);
+  height: 100px;
+  width: 100%;
+}
+
+#temp li {
+  list-style: none;
+}
 
 </style>
