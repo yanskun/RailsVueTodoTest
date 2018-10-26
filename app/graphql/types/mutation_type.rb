9af6946884c7663ed605f3c94bd18369f4b0bca4
Naoya Yasuda <naoya.yasuda@seattleconsulting.co.jp>
2018-10-26 15:26:10 +0900

    add:update_task

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,9 +1,9 @@
 class Types::MutationType < Types::BaseObject
 
-# インプットタイプ仕様に変更
-# 「データ型：String」をTypes::〜〜に修正
-# 入力方式？を変更してるのか？
-
+  # create
+  # インプットタイプ仕様に変更
+  # 「データ型：String」をTypes::〜〜に修正
+  # 入力方式？を変更してるのか？
   field :create_task, TaskType, null: true do
     argument :task, Types::TaskInputType, required: true
   end
@@ -18,5 +18,20 @@ class Types::MutationType < Types::BaseObject
     # IDがキー、nameをバリューでのhashに変換するための「to_h」？
     Task.create task.to_h
   end
+
+  # update
+  # 「Boolean」は戻り値がない、
+  # アップデートはフロントからdbに参照して終わりで良いので
+  # 成功したのかしてないのかわかればそれでよし
+  field :update_task, Boolean, null: false do
+    argument :task, Types::TaskInputType, required: true
+  end
+
+  def update_task(task:)
+    existing = Task.where(id: task[:id]).first
+    existing&.update task.to_h
+  end
+
+  # delete
 
 end
