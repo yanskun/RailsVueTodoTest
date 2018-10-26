c049fc43c1dcc91eac57262fb1506c30e11473f7
Naoya Yasuda <naoya.yasuda@seattleconsulting.co.jp>
2018-10-26 15:02:14 +0900

    fix:InputTypeb create_task

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,11 +1,22 @@
 class Types::MutationType < Types::BaseObject
 
-  field :create_task, Types::TaskType, null: true do
-    argument :name, String, required: true
+# インプットタイプ仕様に変更
+# 「データ型：String」をTypes::〜〜に修正
+# 入力方式？を変更してるのか？
+
+  field :create_task, TaskType, null: true do
+    argument :task, Types::TaskInputType, required: true
   end
 
-  def create_task(name:)
-    Task.create name: name
+  # InputTypes実装前
+  # def create_task(name:)
+  #   Task.create name: name
+  # end
+
+  # 実装後
+  def create_task(task:)
+    # IDがキー、nameをバリューでのhashに変換するための「to_h」？
+    Task.create task.to_h
   end
 
 end
